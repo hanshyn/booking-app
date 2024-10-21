edb46c4ea7b6ef223b38b7895934e79b37264216
Dmytro Hanshyn <di.hanshyn@gmail.com>
2024-10-21 21:19:55 +0300

    Add PaymentSuccessDto for success endpoint

--- src/main/java/com/booking/bookingapp/dto/payment/PaymentRequestDto.java
+++ src/main/java/com/booking/bookingapp/dto/payment/PaymentRequestDto.java
@@ -1,2 +1,11 @@
-package com.booking.bookingapp.dto.payment;public class PaymentRequestDto {
+package com.booking.bookingapp.dto.payment;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record PaymentRequestDto(
+        @NotNull
+        @Min(value = 1)
+        Long bookingId
+) {
 }
---
+++ src/main/java/com/booking/bookingapp/exception/PaymentException.java
@@ -0,0 +1,2 @@
+package com.booking.bookingapp.exception;public class PaymentEcxeption {
+}
