86e4d1c70674dab1bc5b98855c33a039d7e6b960
Dmytro Hanshyn <di.hanshyn@gmail.com>
2025-04-29 19:00:51 +0300

    Added readme file

--- src/main/java/com/booking/bookingapp/config/OpenApiConfig.java
+++ src/main/java/com/booking/bookingapp/config/OpenApiConfig.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Booking API", version = "v1"),
+@OpenAPIDefinition(info = @Info(title = "Booking Service API", version = "v1"),
         security = @SecurityRequirement(name = "bearerAuth"))
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT",
         scheme = "bearer")
